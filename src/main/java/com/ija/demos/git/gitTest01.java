c4c17f56f42e3f52bdfe063cf9cc308de062c805
Ijaz Ahmed <ijaxahmed@gmail.com>
2015-02-14 15:22:10 +0100

    initial commit

---
+++ src/main/java/com/ija/demos/git/gitTest01.java
@@ -0,0 +1,18 @@
+package com.ija.demos.git;
+
+/**
+ * Created by Ijaz on 14/02/2015.
+ */
+public class gitTest01 {
+
+    public static void main (String args[]) {
+
+        new gitTest01().method01();
+
+    }
+
+
+   public void  method01(){
+        System.out.println("I am from gitTest01:method01()");
+    }
+}
---
+++ src/main/java/com/ija/demos/git/gitTest02.java
@@ -0,0 +1,18 @@
+package com.ija.demos.git;
+
+/**
+ * Created by Ijaz on 14/02/2015.
+ */
+public class gitTest02 {
+
+    public static void main (String args[]) {
+
+        new gitTest02().method01();
+
+    }
+
+    public void  method01(){
+        System.out.println("I am from gitTest02:method01()");
+    }
+
+}
