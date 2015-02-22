a3093578362ea8f9070d6b22eedf90f9e6c06151
ijaz ahmed <ijaxahmed@gmail.com>
2015-02-22 18:31:16 +0100

    branch01 first commit

--- src/main/java/com/ija/demos/git/gitTest03.java
+++ src/main/java/com/ija/demos/git/gitTest03.java
@@ -4,4 +4,16 @@ package com.ija.demos.git;
  * Created by Ijaz on 14/02/2015.
  */
 public class gitTest03 {
+
+
+    public static void main (String args[]) {
+
+        new gitTest03().method01();
+
+    }
+
+    public void  method01(){
+        System.out.println("I am from gitTest03:method01()");
+    }
+
 }
