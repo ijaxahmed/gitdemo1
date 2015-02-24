f4da144c8c0456fca90426dbc5095012b45daf40
ijaz ahmed <ijaxahmed@gmail.com>
2015-02-24 23:48:03 +0100

    branch04 first commit

---
+++ src/main/java/com/ija/demos/git/gitTest04.java
@@ -0,0 +1,23 @@
+package com.ija.demos.git;
+
+/**
+ * Created by Ijaz on 24/02/2015.
+ */
+public class gitTest04 {
+
+
+    public static void main (String args[]) {
+
+        new gitTest04().method01();
+
+    }
+
+    public void  method01(){
+        System.out.println("hello, I am from gitTest04:method01()");
+    }
+
+
+
+
+
+}
